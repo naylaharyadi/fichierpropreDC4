d0766dd8aff9fdbdbd620aaf9d1c54cae0ad9dd3
naylaharyadi <haryadinayla@outlook.Com>
2024-03-25 14:55:24 +0100

    Suppression lignes cellules vides

--- cas.py
+++ cas.py
@@ -1,3 +1,5 @@
 import pandas as pd
 
-df = pd.read_csv('test.csv').dropna()
+df = pd.read_csv('test.csv')
+df = df.drop(columns=['Suppressed', 'Series_reference', 'STATUS', 'UNITS', 'Magnitude','Subject', 'Group', 'Series_title_1', 'Series_title_3', 'Series_title_4', 'Series_title_5'])
+df.to_csv('test.csv', index=False)
