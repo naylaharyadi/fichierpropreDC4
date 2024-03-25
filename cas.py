b0fdaaca2740a7f9d12ab38fbbf17059dbbf1a76
naylaharyadi <haryadinayla@outlook.Com>
2024-03-25 15:44:16 +0100

    Done

--- cas.py
+++ cas.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
-df = pd.read_csv('test.csv')
-df = df.drop(columns=['Suppressed', 'Series_reference', 'STATUS', 'UNITS', 'Magnitude','Subject', 'Group', 'Series_title_1', 'Series_title_3', 'Series_title_4', 'Series_title_5'])
+df = pd.read_csv('test (2).csv')
+df = df.drop(columns=['Suppressed', 'Series_reference', 'STATUS', 'UNITS', 'Magnitude','Subject', 'Group', 'Series_title_1', 'Series_title_3', 'Series_title_4', 'Series_title_5']).dropna()
 df.to_csv('test.csv', index=False)
